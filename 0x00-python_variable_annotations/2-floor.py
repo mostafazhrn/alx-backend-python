f325998644a743104611de5044192554a58885cd
mostafazhrn <mostafazhrn@gmail.com>
2024-06-06 07:38:28 -0400

    push_task2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This is a module that defines a function floor"""
+
+
+def floor(n: float) -> int:
+    """ This function shall take a float and return its floor"""
+    return (int(n))
