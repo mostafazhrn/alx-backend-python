d6ac54c92120c5a01a5375ba52ce0c12a8058149
mostafazhrn <mostafazhrn@gmail.com>
2024-06-06 07:45:12 -0400

    push_task4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This is a module that defines variables with annotations"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "holberton"
