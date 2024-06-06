9ceeb46123529ff8a3b6ecac46a8a64c0959c353
mostafazhrn <mostafazhrn@gmail.com>
2024-06-06 07:41:17 -0400

    push_task3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This is a module that defines a function to_str"""
+
+
+def to_str(n: float) -> str:
+    """ This function shall take float & return its string representation"""
+    return (str(n))
