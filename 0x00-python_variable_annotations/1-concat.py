20db52b92c1866947528433364e4de6ca0d9aab6
mostafazhrn <mostafazhrn@gmail.com>
2024-06-06 07:34:42 -0400

    push_task1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This is a module that defines a function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ This function shall take two strings and return their concatenation"""
+    return (str1 + str2)
