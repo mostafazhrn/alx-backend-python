cb3f344bd8ec29fada10cfe5251fe66914276d6d
mostafazhrn <mostafazhrn@gmail.com>
2024-06-10 11:57:18 -0400

    push_taks2_fix

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -2,12 +2,14 @@
 """This module shall define the function of measure_time"""
 import asyncio
 import time
+from typing import List
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay: int) -> float:
+def measure_time(max_delay: int = 10, n: int = 0) -> float:
     """ This function shall take an integer and return a float"""
     start = time.perf_counter()
-    asyncio.run(wait_n(n, max_delay))
-    end = time.perf_counter()
-    return end / n
+    asyncio.run(wait_n(max_delay, n))
+    end = time.perf_counter() - start
+    sum = end / n
+    return sum
