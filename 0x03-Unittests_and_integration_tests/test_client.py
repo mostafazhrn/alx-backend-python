ece864228a04fe0bf308204a0ce9e31520ca4175
mostafazhrn <mostafazhrn@gmail.com>
2024-06-30 12:55:13 -0400

    push_task4

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" This script is a unitest module for testing utils.py """
+import unittest
+from parameterized import parameterized
+from client import GithubOrgClient
+from unittest.mock import patch, Mock, PropertyMock
+from fixtures import TEST_PAYLOAD
+from urllib.error import HTTPError
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ This instance shall rep a class for testin test_client.py """
+    @parameterized.expand([
+        ("google"),
+        ("abc")
+        ])
+    @patch('client.get_json', return_value={"payload": True})
+    def test_org(self, org_name, mock_get_json):
+        """ This method is to test org """
+        valid_op = GithubOrgClient(org_name)
+        return_res = valid_op.org
+        self.assertEqual(return_res, mock_get_json.return_value)
+        mock_get_json.assert_called_once()
